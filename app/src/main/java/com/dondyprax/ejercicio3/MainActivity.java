d872620d7bfb99962a11ff2ea664c2c9038957c6
Ricardo-1998 <00004817@uca.edu.sv>
2019-03-15 16:42:59 -0600

    terminado

--- app/src/main/java/com/dondyprax/ejercicio3/MainActivity.java
+++ app/src/main/java/com/dondyprax/ejercicio3/MainActivity.java
@@ -16,6 +16,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button boton1;
     Button boton2;
     Button boton3;
+    ArrayList<Drawable> rojos = new ArrayList<>();
+    ArrayList<Drawable> azules = new ArrayList<>();
+    ArrayList<Drawable> verdes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +35,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        ArrayList<Integer> rojos = new ArrayList<>();
-        ArrayList<Integer> azules = new ArrayList<>();
-        ArrayList<Integer> verdes = new ArrayList<>();
-
-        azules.add(getResources().getColor(R.color.blue));
-        azules.add(getResources().getColor(R.color.blue2));
-        azules.add(getResources().getColor(R.color.blue3));
-        azules.add(getResources().getColor(R.color.blue4));
-        azules.add(getResources().getColor(R.color.blue5));
-        azules.add(getResources().getColor(R.color.blue6));
-
+        agregarColores();
+        int n=randomColor();
         int viewId = v.getId();
         Log.i("id", viewId+"");
         switch (viewId){
             case R.id.boton1:
-                Log.d("color", azules.get(0)+"");
-                boton1.setBackground(getResources().getColor(azules.get(0)));
+                Log.d("color", rojos.get(0)+"");
+                boton1.setBackground(rojos.get(n));
                 break;
             case R.id.boton2:
-                boton2.setBackground(getResources().getDrawable(azules.get(1)));
+                Log.d("color", verdes.get(0)+"");
+                boton2.setBackground(verdes.get(n));
                 break;
             case R.id.boton3:
-                boton3.setBackground(getResources().getDrawable(azules.get(1)));
+                Log.d("color", azules.get(0)+"");
+                boton3.setBackground(azules.get(n));
                 break;
         }
+    }
+
+    public void agregarColores(){
+        azules.add(getResources().getDrawable(R.color.blue));
+        azules.add(getResources().getDrawable(R.color.blue2));
+        azules.add(getResources().getDrawable(R.color.blue3));
+        azules.add(getResources().getDrawable(R.color.blue4));
+        azules.add(getResources().getDrawable(R.color.blue5));
+        azules.add(getResources().getDrawable(R.color.blue6));
+
+        rojos.add(getResources().getDrawable(R.color.red));
+        rojos.add(getResources().getDrawable(R.color.red2));
+        rojos.add(getResources().getDrawable(R.color.red3));
+        rojos.add(getResources().getDrawable(R.color.red4));
+        rojos.add(getResources().getDrawable(R.color.red5));
+        rojos.add(getResources().getDrawable(R.color.red6));
+
+        verdes.add(getResources().getDrawable(R.color.green));
+        verdes.add(getResources().getDrawable(R.color.green2));
+        verdes.add(getResources().getDrawable(R.color.green3));
+        verdes.add(getResources().getDrawable(R.color.green4));
+        verdes.add(getResources().getDrawable(R.color.green5));
+        verdes.add(getResources().getDrawable(R.color.green6));
+    }
+
+    public int randomColor(){
+        int num= (int) (Math.random()*6)+1;
+        return num;
+
     }
 }
